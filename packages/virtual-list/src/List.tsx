5d0dac6bbf270950934c6a7dd6cf940489c65c65
aibayanyu20 <aibayanyu@qq.com>
2023-12-15 17:55:45 +0800

    chore: change

--- packages/virtual-list/src/List.tsx
+++ packages/virtual-list/src/List.tsx
@@ -1,7 +1,12 @@
-import type { CSSProperties } from 'vue'
+import type { CSSProperties, DefineComponent, VNode, VNodeChild } from 'vue'
+import { computed, defineComponent, shallowRef } from 'vue'
+import classNames from 'classnames'
 import type { ScrollPos, ScrollTarget } from './hooks/useScrollTo'
+import type { ExtraRenderInfo, GetKey, Key, SharedConfig } from './interface.ts'
+import type { ScrollBarDirectionType } from './ScrollBar.tsx'
+import type { InnerProps } from './Filter.tsx'
 
-const EMPTY_DATA = []
+const EMPTY_DATA = [] as any[]
 
 const ScrollStyle: CSSProperties = {
   overflowY: 'auto',
@@ -21,3 +26,106 @@ export interface ListRef {
   scrollTo: ScrollTo
   getScrollInfo: () => ScrollInfo
 }
+
+export interface ListProps<T> {
+  prefixCls?: string
+  data: T[]
+  height?: number
+  itemHeight?: number
+  /** If not match virtual scroll condition, Set List still use height of container. */
+  fullHeight?: boolean
+  itemKey: Key | ((item: T) => Key)
+  component?: string | VNode | DefineComponent<any>
+  /** Set `false` will always use real scroll instead of virtual one */
+  virtual?: boolean
+  direction?: ScrollBarDirectionType
+  /**
+   * By default `scrollWidth` is same as container.
+   * When set this, it will show the horizontal scrollbar and
+   * `scrollWidth` will be used as the real width instead of container width.
+   * When set, `virtual` will always be enabled.
+   */
+  scrollWidth?: number
+
+  styles?: {
+    horizontalScrollBar?: CSSProperties
+    horizontalScrollBarThumb?: CSSProperties
+    verticalScrollBar?: CSSProperties
+    verticalScrollBarThumb?: CSSProperties
+  }
+
+  onScroll?: (...args: any[]) => void
+
+  /**
+   * Given the virtual offset value.
+   * It's the logic offset from start position.
+   */
+  onVirtualScroll?: (info: ScrollInfo) => void
+
+  /** Trigger when render list item changed */
+  onVisibleChange?: (visibleList: T[], fullList: T[]) => void
+
+  /** Inject to inner container props. Only use when you need pass aria related data */
+  innerProps?: InnerProps
+
+  /** Render extra content into Filler */
+  extraRender?: (info: ExtraRenderInfo) => VNodeChild
+}
+
+const List = defineComponent<ListProps<any>>({
+  name: 'List',
+  setup(props, { attrs }) {
+    // ================================= MISC =================================
+    const useVirtual = computed(() => !!(props.virtual !== false && props.height && props.itemHeight))
+    const inVirtual = computed(() => useVirtual.value && !!props.data.length && (props.itemHeight! * props.data.length > props.height! || !!props.scrollWidth))
+    const isRTL = computed(() => props.direction === 'rtl')
+
+    const componentRef = shallowRef<HTMLDivElement>()
+    const fillerInnerRef = shallowRef<HTMLDivElement>()
+    // =============================== Item Key ===============================
+
+    const offsetTop = shallowRef(0)
+    const offsetLeft = shallowRef(0)
+    const scrollMoving = shallowRef(false)
+    const onScrollbarStartMove = () => {
+      scrollMoving.value = true
+    }
+
+    const onScrollbarStopMove = () => {
+      scrollMoving.value = false
+    }
+    // =============================== Item Key ===============================
+    const getKey = <T = any>(item: T): GetKey<any> => {
+      if (typeof props.itemKey === 'function')
+        return props.itemKey(item) as any
+      return (item as any)?.[props.itemKey]
+    }
+
+    const sharedConfig: SharedConfig<any> = {
+      getKey,
+    } as any
+
+    function syncScrollTop(newTop: number | ((prev: number) => number)) {
+      let value: number
+      if (typeof newTop === 'function')
+        value = newTop(offsetTop.value)
+
+      else
+        value = newTop
+
+      // const alignedTop =
+      // componentRef.value?.scrollTop = value
+    }
+
+    return () => {
+      const { prefixCls = 'rc-virtual-list', data } = props
+      const mergedClassName = classNames(prefixCls, {
+        [`${prefixCls}-rtl`]: isRTL.value,
+      }, attrs.class as any)
+      const mergeData = data || EMPTY_DATA
+      return <div>list</div>
+    }
+  },
+})
+
+export default List
---
+++ packages/virtual-list/src/hooks/useScrollTo.tsx
@@ -0,0 +1,181 @@
+import type { Ref } from 'vue'
+import { shallowRef, watchSyncEffect } from 'vue'
+import { warning } from '@vue-components/util'
+import raf from '@vue-components/util/raf'
+import type CacheMap from '../utils/CacheMap.ts'
+import type { GetKey, Key } from '../interface.ts'
+
+const MAX_TIMES = 10
+
+export type ScrollAlign = 'top' | 'bottom' | 'auto'
+
+export interface ScrollPos {
+  left?: number
+  top?: number
+}
+
+export type ScrollTarget =
+    | {
+      index: number
+      align?: ScrollAlign
+      offset?: number
+    }
+    | {
+      key: Key
+      align?: ScrollAlign
+      offset?: number
+    }
+
+export default function useScrollTo<T>(
+  containerRef: Ref<HTMLDivElement>,
+  data: Ref<T[]>,
+  heights: CacheMap,
+  itemHeight: Ref<number>,
+  getKey: GetKey<T>,
+  collectHeight: () => void,
+  syncScrollTop: (newTop: number) => void,
+  triggerFlash: () => void,
+) {
+  const scrollRef = shallowRef<number>()
+
+  const syncState = shallowRef<{
+    times: number
+    index: number
+    offset: number
+    originAlign: ScrollAlign
+    targetAlign?: 'top' | 'bottom' | null
+    lastTop?: number
+  }>()
+
+  const effectLayout = () => {
+    if (syncState.value && syncState.value.times < MAX_TIMES) {
+      if (!containerRef.value) {
+        syncState.value = { ...syncState.value }
+        return
+      }
+      collectHeight()
+      const { targetAlign, originAlign, index, offset } = syncState.value!
+      const height = containerRef.value.clientHeight
+      let needCollectHeight = false
+      let newTargetAlign: 'top' | 'bottom' | null = targetAlign!
+      let targetTop: number | null = null
+
+      // Go to next frame if height not exist
+      if (height) {
+        const mergedAlign = targetAlign || originAlign
+
+        // Get top & bottom
+        let stackTop = 0
+        let itemTop = 0
+        let itemBottom = 0
+
+        const maxLen = Math.min(data.value.length - 1, index)
+
+        for (let i = 0; i <= maxLen; i += 1) {
+          const key = getKey(data.value[i])
+          itemTop = stackTop
+          const cacheHeight = heights.get(key)
+          itemBottom = itemTop + (cacheHeight === undefined ? itemHeight.value : cacheHeight)
+
+          stackTop = itemBottom
+        }
+
+        // Check if need sync height (visible range has item not record height)
+        let leftHeight = mergedAlign === 'top' ? offset : height - offset
+        for (let i = maxLen; i >= 0; i -= 1) {
+          const key = getKey(data.value[i])
+          const cacheHeight = heights.get(key)
+
+          if (cacheHeight === undefined) {
+            needCollectHeight = true
+            break
+          }
+
+          leftHeight -= cacheHeight
+          if (leftHeight <= 0)
+            break
+        }
+
+        // Scroll to
+        switch (mergedAlign) {
+          case 'top':
+            targetTop = itemTop - offset
+            break
+          case 'bottom':
+            targetTop = itemBottom - height + offset
+            break
+
+          default: {
+            const { scrollTop } = containerRef.value
+            const scrollBottom = scrollTop + height
+            if (itemTop < scrollTop)
+              newTargetAlign = 'top'
+            else if (itemBottom > scrollBottom)
+              newTargetAlign = 'bottom'
+          }
+        }
+
+        if (targetTop !== null)
+          syncScrollTop(targetTop)
+
+        // One more time for sync
+        if (targetTop !== syncState.value!.lastTop)
+          needCollectHeight = true
+
+        // Trigger next effect
+        if (needCollectHeight) {
+          syncState.value = {
+            ...syncState.value,
+            times: syncState.value!.times + 1,
+            targetAlign: newTargetAlign!,
+            lastTop: targetTop!,
+          } as any
+        }
+      }
+    }
+
+    else if (process.env.NODE_ENV !== 'production' && syncState.value?.times === MAX_TIMES) {
+      warning(
+        false,
+        'Seems `scrollTo` with `vc-virtual-list` reach the max limitation. Please fire issue for us. Thanks.',
+      )
+    }
+  }
+  watchSyncEffect(() => {
+    if (containerRef.value || syncState.value)
+      effectLayout()
+  })
+  // =========================== Scroll To ===========================
+  return (arg: any) => {
+    // When not argument provided, we think dev may want to show the scrollbar
+    if (arg === null || arg === undefined) {
+      triggerFlash()
+      return
+    }
+
+    // Normal scroll logic
+    raf.cancel(scrollRef.value!)
+
+    if (typeof arg === 'number') {
+      syncScrollTop(arg)
+    }
+    else if (arg && typeof arg === 'object') {
+      let index: number
+      const { align } = arg
+
+      if ('index' in arg)
+        ({ index } = arg)
+      else
+        index = data.value.findIndex(item => getKey(item) === arg.key)
+
+      const { offset = 0 } = arg
+
+      syncState.value = {
+        times: 0,
+        index,
+        offset,
+        originAlign: align,
+      }
+    }
+  }
+}
