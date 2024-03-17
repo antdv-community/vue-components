3987dff1019a544d44b86a43792b6f15928af5c2
aibayanyu20 <aibayanyu@qq.com>
2024-03-17 14:15:20 +0800

    fix: change

---
+++ packages/context/src/context.ts
@@ -0,0 +1,42 @@
+import type { DefineSetupFnComponent, InjectionKey } from 'vue'
+import { defineComponent, inject, provide } from 'vue'
+
+const globalContext = new Map<SelectorContext<any>, any>()
+
+export interface SelectorContext<ContextProps> {
+  Context: InjectionKey<ContextProps>
+  Provider: DefineSetupFnComponent<{ value: any }>
+  defaultValue?: ContextProps
+}
+
+export function createContext<ContextProps>(defaultValue?: ContextProps): SelectorContext<ContextProps> {
+  const Context: InjectionKey<ContextProps> = Symbol('Context')
+  const Provider = defineComponent((props, { slots }) => {
+    provide(Context, props.value)
+    return () => {
+      return slots.default?.()
+    }
+  }, {
+    props: {
+      value: {
+        type: Object,
+        default: () => defaultValue,
+      },
+    },
+  })
+
+  const state = {
+    Provider,
+    Context,
+    defaultValue,
+  }
+  if (!globalContext.has(state))
+    globalContext.set(state, defaultValue)
+
+  return state
+}
+
+export function useContext<ContextProps>(holder: SelectorContext<ContextProps>) {
+  const context = holder.Context
+  return inject(context, (globalContext.get(holder) ?? holder.defaultValue) as ContextProps)
+}
---
+++ packages/context/vite.config.ts
@@ -0,0 +1,18 @@
+import type { UserConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
+import fg from 'fast-glob'
+import { buildCommon } from '../../scripts/build.common'
+
+const entry = fg.sync(['src/**/*.ts', 'src/**/*.tsx', '!src/**/*.test.ts', '!src/**/*.test.tsx', '!src/**/tests'])
+
+export default defineConfig({
+  ...mergeConfig(buildCommon({
+    external: ['vue'],
+  }), {
+    build: {
+      lib: {
+        entry,
+      },
+    },
+  } as UserConfig),
+})
