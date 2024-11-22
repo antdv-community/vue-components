62ea43bcc2001feea852272867975de0bc839c99
carl-chen <ccheartszzz@gmail.com>
2024-11-22 15:51:50 +0800

    perf: optimize

--- packages/steps/src/Step.tsx
+++ packages/steps/src/Step.tsx
@@ -17,14 +17,8 @@ export function generatorStepProps() {
     iconPrefix: String,
     active: Boolean,
     disabled: Boolean,
-    stepIndex: {
-      type: Number,
-      required: true,
-    },
-    stepNumber: {
-      type: Number,
-      required: true,
-    },
+    stepIndex: Number,
+    stepNumber: Number,
     status: String as PropType<Status>,
     title: [String, Object, Function] as PropType<VueNode>,
     subTitle: [String, Object, Function] as PropType<VueNode>,
@@ -41,134 +35,135 @@ export function generatorStepProps() {
   }
 }
 
-export default defineComponent((props, { attrs }) => {
-  const clickable = computed(() => !!props.onStepClick && !props.disabled)
+export default defineComponent({
+  props: generatorStepProps(),
+  setup(props, { attrs }) {
+    const clickable = computed(() => !!props.onStepClick && !props.disabled)
 
-  const accessibilityProps = computed(() => {
-    if (clickable.value) {
-      return {
-        role: 'button',
-        tabIndex: 0,
-        onClick: (e: MouseEvent) => {
-          props.onClick?.(e)
-          props.onStepClick?.(props.stepIndex)
-        },
-        onKeydown: (e: KeyboardEvent) => {
-          const { which } = e
-          if (which === KeyCode.ENTER || which === KeyCode.SPACE) {
-            props.onStepClick?.(props.stepIndex)
-          }
-        },
+    const accessibilityProps = computed(() => {
+      if (clickable.value) {
+        return {
+          role: 'button',
+          tabIndex: 0,
+          onClick: (e: MouseEvent) => {
+            props.onClick?.(e)
+            props.onStepClick?.(props.stepIndex!)
+          },
+          onKeydown: (e: KeyboardEvent) => {
+            const { which } = e
+            if (which === KeyCode.ENTER || which === KeyCode.SPACE) {
+              props.onStepClick?.(props.stepIndex!)
+            }
+          },
+        }
       }
-    }
 
-    return {}
-  })
-
-  const renderIconNode = () => {
-    const { stepIcon, stepNumber, title, description, progressDot, prefixCls, iconPrefix, status, icons, icon } = props
-    let iconNode: VueNode
-    const iconClassName = classNames(`${prefixCls}-icon`, `${prefixCls}icon`, {
-      [`${iconPrefix}icon-${icon}`]: icon && isString(icon),
-      [`${iconPrefix}icon-check`]:
-        !icon && status === 'finish' && ((icons && !icons.finish) || !icons),
-      [`${iconPrefix}icon-cross`]:
-        !icon && status === 'error' && ((icons && !icons.error) || !icons),
+      return {}
     })
 
-    const iconDot = <span class={`${prefixCls}-icon-dot`} />
+    const renderIconNode = () => {
+      const { stepIcon, stepNumber, title, description, progressDot, prefixCls, iconPrefix, status, icons, icon } = props
+      let iconNode: VueNode
+      const iconClassName = classNames(`${prefixCls}-icon`, `${prefixCls}icon`, {
+        [`${iconPrefix}icon-${icon}`]: icon && isString(icon),
+        [`${iconPrefix}icon-check`]:
+          !icon && status === 'finish' && ((icons && !icons.finish) || !icons),
+        [`${iconPrefix}icon-cross`]:
+          !icon && status === 'error' && ((icons && !icons.error) || !icons),
+      })
 
-    if (progressDot) {
-      if (typeof progressDot === 'function') {
-        iconNode = (
-          <span class={`${prefixCls}-icon`}>
-            {progressDot(iconDot, {
-              index: stepNumber - 1,
-              status: status!,
-              title,
-              description,
-            })}
-          </span>
-        )
+      const iconDot = <span class={`${prefixCls}-icon-dot`} />
+
+      if (progressDot) {
+        if (typeof progressDot === 'function') {
+          iconNode = (
+            <span class={`${prefixCls}-icon`}>
+              {progressDot(iconDot, {
+                index: stepNumber! - 1,
+                status: status!,
+                title,
+                description,
+              })}
+            </span>
+          )
+        }
+        else {
+          iconNode = <span class={`${prefixCls}-icon`}>{iconDot}</span>
+        }
+      }
+      else if (icon && !isString(icon)) {
+        iconNode = <span class={`${prefixCls}-icon`}>{icon}</span>
+      }
+      else if (icons && icons.finish && status === 'finish') {
+        iconNode = <span class={`${prefixCls}-icon`}>{icons.finish}</span>
+      }
+      else if (icons && icons.error && status === 'error') {
+        iconNode = <span class={`${prefixCls}-icon`}>{icons.error}</span>
+      }
+      else if (icon || status === 'finish' || status === 'error') {
+        iconNode = <span class={iconClassName} />
       }
       else {
-        iconNode = <span class={`${prefixCls}-icon`}>{iconDot}</span>
+        iconNode = <span class={`${prefixCls}-icon`}>{stepNumber}</span>
       }
-    }
-    else if (icon && !isString(icon)) {
-      iconNode = <span class={`${prefixCls}-icon`}>{icon}</span>
-    }
-    else if (icons && icons.finish && status === 'finish') {
-      iconNode = <span class={`${prefixCls}-icon`}>{icons.finish}</span>
-    }
-    else if (icons && icons.error && status === 'error') {
-      iconNode = <span class={`${prefixCls}-icon`}>{icons.error}</span>
-    }
-    else if (icon || status === 'finish' || status === 'error') {
-      iconNode = <span class={iconClassName} />
-    }
-    else {
-      iconNode = <span class={`${prefixCls}-icon`}>{stepNumber}</span>
+
+      if (stepIcon) {
+        iconNode = stepIcon({
+          index: stepNumber! - 1,
+          status: status!,
+          title,
+          description,
+          node: iconNode,
+        })
+      }
+
+      return iconNode
     }
 
-    if (stepIcon) {
-      iconNode = stepIcon({
-        index: stepNumber - 1,
-        status: status!,
-        title,
-        description,
-        node: iconNode,
-      })
-    }
+    const mergedStatus = computed(() => props.status || 'wait')
+    return () => {
+      const { prefixCls, className, icon, active, style = {}, disabled, tailContent, onClick, title, subTitle, description, render } = props
+      const classString = classNames(
+        `${prefixCls}-item`,
+        `${prefixCls}-item-${mergedStatus.value}`,
+        className,
+        {
+          [`${prefixCls}-item-custom`]: icon,
+          [`${prefixCls}-item-active`]: active,
+          [`${prefixCls}-item-disabled`]: disabled === true,
+        },
+      )
 
-    return iconNode
-  }
-
-  const mergedStatus = computed(() => props.status || 'wait')
-  return () => {
-    const { prefixCls, className, icon, active, disabled, tailContent, onClick, title, subTitle, description, render } = props
-    const classString = classNames(
-      `${prefixCls}-item`,
-      `${prefixCls}-item-${mergedStatus.value}`,
-      className,
-      {
-        [`${prefixCls}-item-custom`]: icon,
-        [`${prefixCls}-item-active`]: active,
-        [`${prefixCls}-item-disabled`]: disabled === true,
-      },
-    )
-
-    const { style = {}, ...restProps } = attrs
-    const stepItemStyle: CSSProperties = { ...(style as CSSProperties) }
-    let stepNode: VueNode = (
-      <div {...restProps} class={classString} style={stepItemStyle}>
-        <div onClick={onClick} {...accessibilityProps.value} class={`${prefixCls}-item-container`}>
-          <div class={`${prefixCls}-item-tail`}>{tailContent}</div>
-          <div class={`${prefixCls}-item-icon`}>{renderIconNode()}</div>
-          <div class={`${prefixCls}-item-content`}>
-            <div class={`${prefixCls}-item-title`}>
-              {title}
-              {subTitle && (
-                <div
-                  title={typeof subTitle === 'string' ? subTitle : undefined}
-                  class={`${prefixCls}-item-subtitle`}
-                >
-                  {subTitle}
-                </div>
-              )}
+      const { ...restProps } = attrs
+      const stepItemStyle: CSSProperties = { ...(style as CSSProperties) }
+      let stepNode: VueNode = (
+        <div {...restProps} class={classString} style={stepItemStyle}>
+          <div onClick={onClick} {...accessibilityProps.value} class={`${prefixCls}-item-container`}>
+            <div class={`${prefixCls}-item-tail`}>{tailContent}</div>
+            <div class={`${prefixCls}-item-icon`}>{renderIconNode()}</div>
+            <div class={`${prefixCls}-item-content`}>
+              <div class={`${prefixCls}-item-title`}>
+                {title}
+                {subTitle && (
+                  <div
+                    title={typeof subTitle === 'string' ? subTitle : undefined}
+                    class={`${prefixCls}-item-subtitle`}
+                  >
+                    {subTitle}
+                  </div>
+                )}
+              </div>
+              {description && <div class={`${prefixCls}-item-description`}>{description}</div>}
             </div>
-            {description && <div class={`${prefixCls}-item-description`}>{description}</div>}
           </div>
         </div>
-      </div>
-    )
+      )
 
-    if (render) {
-      stepNode = (render(stepNode) || null)
+      if (render) {
+        stepNode = (render(stepNode) || null)
+      }
+
+      return stepNode
     }
-
-    return stepNode
-  }
-}, {
-  props: generatorStepProps(),
+  },
 })
--- packages/steps/src/Steps.tsx
+++ packages/steps/src/Steps.tsx
@@ -8,6 +8,10 @@ import Step from './Step'
 
 type StepProps = InstanceType<typeof Step>['$props']
 
+type Mutable<T> = {
+  -readonly [K in keyof T]: T[K];
+}
+
 export function generatorStepsProps() {
   return {
     prefixCls: {
@@ -65,97 +69,98 @@ export function generatorStepsProps() {
   }
 }
 
-const Steps = defineComponent((props) => {
-  const isNav = computed(() => props.type === 'navigation')
-  const isInline = computed(() => props.type === 'inline')
-
-  const mergedProgressDot = computed(() => isInline.value || props.progressDot)
-  const mergedDirection = computed(() => isInline.value ? 'horizontal' : props.direction)
-  const mergedSize = computed(() => isInline.value ? undefined : props.size)
-
-  const adjustedLabelPlacement = computed(() => mergedProgressDot.value ? 'vertical' : props.labelPlacement)
-
-  const onStepClick = (next: number) => {
-    const { current = 0 } = props
-    if (props.onChange && current !== next) {
-      props.onChange(next)
-    }
-  }
-
-  const renderStep = (item: StepProps, index: number) => {
-    const { stepIcon, icons, onChange, initial, current, iconPrefix, style, itemRender, status, prefixCls } = props
-    const mergedItem: StepProps = { ...item }
-    const stepNumber = initial + index
-
-    // fix tail color
-    if (status === 'error' && index === current - 1) {
-      mergedItem.className = `${prefixCls}-next-error`
-    }
-
-    if (!mergedItem.status) {
-      if (stepNumber === current) {
-        mergedItem.status = status as Status
-      }
-      else if (stepNumber < current) {
-        mergedItem.status = 'finish'
-      }
-      else {
-        mergedItem.status = 'wait'
-      }
-    }
-
-    if (isInline.value) {
-      mergedItem.icon = undefined
-      mergedItem.subTitle = undefined
-    }
-
-    if (!mergedItem.render && itemRender) {
-      mergedItem.render = (stepItem: VueNode) => itemRender(mergedItem, stepItem)
-    }
-
-    return (
-      <Step
-        {...mergedItem}
-        active={stepNumber === current}
-        stepNumber={stepNumber + 1}
-        stepIndex={stepNumber}
-        key={stepNumber}
-        prefixCls={prefixCls}
-        iconPrefix={iconPrefix}
-        wrapperStyle={style}
-        progressDot={mergedProgressDot.value}
-        stepIcon={stepIcon}
-        icons={icons}
-        onStepClick={onChange && onStepClick}
-      />
-    )
-  }
-  return () => {
-    const {
-      prefixCls,
-      className,
-      items,
-      style,
-    } = props
-
-    const restProps = omit(props, ['prefixCls', 'className', 'style', 'direction', 'type', 'labelPlacement', 'iconPrefix', 'status', 'size', 'current', 'progressDot', 'stepIcon', 'initial', 'icons', 'onChange', 'itemRender', 'items'])
-
-    const classString = classNames(prefixCls, `${prefixCls}-${mergedDirection.value}`, className, {
-      [`${prefixCls}-${mergedSize.value}`]: mergedSize.value,
-      [`${prefixCls}-label-${adjustedLabelPlacement.value}`]: mergedDirection.value === 'horizontal',
-      [`${prefixCls}-dot`]: !!mergedProgressDot.value,
-      [`${prefixCls}-navigation`]: isNav.value,
-      [`${prefixCls}-inline`]: isInline.value,
-    })
-
-    return (
-      <div class={classString} style={style} {...restProps}>
-        {items.filter(Boolean).map<VueNode>(renderStep)}
-      </div>
-    )
-  }
-}, {
+const Steps = defineComponent({
   props: generatorStepsProps(),
+  setup(props) {
+    const isNav = computed(() => props.type === 'navigation')
+    const isInline = computed(() => props.type === 'inline')
+
+    const mergedProgressDot = computed(() => isInline.value || props.progressDot)
+    const mergedDirection = computed(() => isInline.value ? 'horizontal' : props.direction)
+    const mergedSize = computed(() => isInline.value ? undefined : props.size)
+
+    const adjustedLabelPlacement = computed(() => mergedProgressDot.value ? 'vertical' : props.labelPlacement)
+
+    const onStepClick = (next: number) => {
+      const { current = 0 } = props
+      if (props.onChange && current !== next) {
+        props.onChange(next)
+      }
+    }
+
+    const renderStep = (item: StepProps, index: number) => {
+      const { stepIcon, icons, onChange, initial, current, iconPrefix, style, itemRender, status, prefixCls } = props
+      const mergedItem: Mutable<StepProps> = { ...item }
+      const stepNumber = initial + index
+
+      // fix tail color
+      if (status === 'error' && index === current - 1) {
+        mergedItem.className = `${prefixCls}-next-error`
+      }
+
+      if (!mergedItem.status) {
+        if (stepNumber === current) {
+          mergedItem.status = status as Status
+        }
+        else if (stepNumber < current) {
+          mergedItem.status = 'finish'
+        }
+        else {
+          mergedItem.status = 'wait'
+        }
+      }
+
+      if (isInline.value) {
+        mergedItem.icon = undefined
+        mergedItem.subTitle = undefined
+      }
+
+      if (!mergedItem.render && itemRender) {
+        mergedItem.render = (stepItem: VueNode) => itemRender(mergedItem, stepItem)
+      }
+
+      return (
+        <Step
+          {...mergedItem}
+          active={stepNumber === current}
+          stepNumber={stepNumber + 1}
+          stepIndex={stepNumber}
+          key={stepNumber}
+          prefixCls={prefixCls}
+          iconPrefix={iconPrefix}
+          wrapperStyle={style}
+          progressDot={mergedProgressDot.value}
+          stepIcon={stepIcon}
+          icons={icons}
+          onStepClick={onChange && onStepClick}
+        />
+      )
+    }
+    return () => {
+      const {
+        prefixCls,
+        className,
+        items,
+        style,
+      } = props
+
+      const restProps = omit(props, ['prefixCls', 'className', 'style', 'direction', 'type', 'labelPlacement', 'iconPrefix', 'status', 'size', 'current', 'progressDot', 'stepIcon', 'initial', 'icons', 'onChange', 'itemRender', 'items'])
+
+      const classString = classNames(prefixCls, `${prefixCls}-${mergedDirection.value}`, className, {
+        [`${prefixCls}-${mergedSize.value}`]: mergedSize.value,
+        [`${prefixCls}-label-${adjustedLabelPlacement.value}`]: mergedDirection.value === 'horizontal',
+        [`${prefixCls}-dot`]: !!mergedProgressDot.value,
+        [`${prefixCls}-navigation`]: isNav.value,
+        [`${prefixCls}-inline`]: isInline.value,
+      })
+
+      return (
+        <div class={classString} style={style} {...restProps}>
+          {items.filter(Boolean).map<VueNode>(renderStep)}
+        </div>
+      )
+    }
+  },
 })
 
 export default Steps
