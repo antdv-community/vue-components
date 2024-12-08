e2ea25cbd36b03eca3a9decd56f3dfe5110968ed
carl chen <ccheartszzz@gmail.com>
2024-12-08 21:09:12 +0800

    wip: fix class

--- packages/collapse/src/Panel.tsx
+++ packages/collapse/src/Panel.tsx
@@ -1,14 +1,66 @@
-// import { classnames } from 'classnames'
+import type { HTMLAttributes } from 'vue'
+import classnames from 'classnames'
 import { computed, defineComponent } from 'vue'
 import { generatorCollapsePanelProps } from './interface'
 
 const CollapsePanel = defineComponent({
   name: 'CollapsePanel',
   props: generatorCollapsePanelProps(),
-  setup(props) {
+  setup(props, { attrs }) {
     const disabled = computed(() => props.collapsible === 'disabled')
-    return () => {
+    const ifExtraExist = computed(() => props.extra !== null && props.extra !== undefined && typeof props.extra !== 'boolean')
 
+    const collapsibleProps = computed(() => {
+      return {
+        onClick: () => {
+          props.onItemClick?.(props.panelKey!)
+        },
+      }
+    })
+
+    return () => {
+      const { prefixCls, isActive, className, headerClass, collapsible, classNemes: customizeClassNames = {}, showArrow, styles = {} } = props
+      const { ...restProps } = attrs
+      const collapsePanelClassNames = classnames(
+        `${prefixCls}-item`,
+        {
+          [`${prefixCls}-item-active`]: isActive,
+          [`${prefixCls}-item-disabled`]: disabled.value,
+        },
+        className,
+      )
+      const headerClassName = classnames(
+        headerClass,
+        `${prefixCls}-header`,
+        {
+          [`${prefixCls}-collapsible-${collapsible}`]: !!collapsible,
+        },
+        customizeClassNames.header,
+      )
+
+      const headerProps: HTMLAttributes = {
+        class: headerClassName,
+        style: styles.header,
+      }
+
+      // ======================== Icon ========================
+      const iconNodeInner
+    = typeof expandIcon === 'function' ? expandIcon(props) : <i class="arrow" />
+      const iconNode = iconNodeInner && (
+        <div
+          class={`${prefixCls}-expand-icon`}
+          {...(['header', 'icon'].includes(collapsible!) ? collapsibleProps : {})}
+        >
+          {iconNodeInner}
+        </div>
+      )
+
+      return (
+        <div {...restProps} class={collapsePanelClassNames}>
+          <div {...headerProps}>
+          </div>
+        </div>
+      )
     }
   },
 })
