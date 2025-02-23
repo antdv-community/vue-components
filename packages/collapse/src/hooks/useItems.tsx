b8ff77337abbe329d62bcd112be4d12832aef301
carl chen <ccheartszzz@gmail.com>
2025-02-23 23:50:01 +0800

    wip: add getNewChild

--- packages/collapse/src/hooks/useItems.tsx
+++ packages/collapse/src/hooks/useItems.tsx
@@ -1,9 +1,12 @@
+import type { RendererElement, RendererNode, VNode, VNodeNormalizedChildren } from 'vue'
 import type {
   CollapsePanelProps,
   CollapseProps,
   ItemType,
   Key,
 } from '../interface'
+import { flattenChildren, isEmptyElement } from '@v-c/util/dist/props-util'
+import { cloneElement } from '@v-c/util/dist/vnode'
 import CollapsePanel from '../Panel'
 
 type Props = Pick<
@@ -86,8 +89,98 @@ function convertItemsToNodes(items: ItemType[], props: Props) {
   })
 }
 
-export function useItems(items?: ItemType[], props?: Props) {
+/**
+ * @deprecated The next major version will be removed
+ */
+function getNewChild(
+  child: VNode<RendererNode, RendererElement, CollapsePanelProps>,
+  index: number,
+  props: Props,
+) {
+  if (isEmptyElement(child) || !child)
+    return null
+
+  if (
+    typeof child === 'boolean'
+    || typeof child === 'number'
+    || typeof child === 'string'
+  ) {
+    return child
+  }
+
+  const {
+    prefixCls,
+    accordion,
+    collapsible,
+    destroyInactivePanel,
+    onItemClick,
+    activeKey,
+    openMotion,
+    expandIcon,
+    classNames: collapseClassNames,
+    styles,
+  } = props
+
+  const key = child.key || String(index)
+
+  const {
+    header,
+    headerClass,
+    destroyInactivePanel: childDestroyInactivePanel,
+    collapsible: childCollapsible,
+    onItemClick: childOnItemClick,
+  } = child.props || {}
+
+  let isActive = false
+  if (accordion) {
+    isActive = activeKey[0] === key
+  }
+  else {
+    isActive = activeKey.includes(key as Key)
+  }
+
+  const mergeCollapsible = childCollapsible ?? collapsible
+
+  const handleItemClick = (value: Key) => {
+    if (mergeCollapsible === 'disabled')
+      return
+    onItemClick?.(value)
+    childOnItemClick?.(value)
+  }
+
+  const childProps = {
+    key,
+    panelKey: key,
+    header,
+    headerClass,
+    classNames: collapseClassNames,
+    styles,
+    isActive,
+    prefixCls,
+    destroyInactivePanel: childDestroyInactivePanel ?? destroyInactivePanel,
+    openMotion,
+    accordion,
+    children: child.props?.children,
+    onItemClick: handleItemClick,
+    expandIcon,
+    collapsible: mergeCollapsible,
+  }
+
+  Object.keys(childProps).forEach((propName) => {
+    if (
+      typeof childProps[propName as keyof typeof childProps] === 'undefined'
+    ) {
+      delete childProps[propName as keyof typeof childProps]
+    }
+  })
+
+  return cloneElement(child, childProps)
+}
+
+export function useItems(items?: ItemType[], children?: () => VNode | VNodeNormalizedChildren, props?: Props) {
   if (Array.isArray(items)) {
     return convertItemsToNodes(items, props!)
   }
+
+  return flattenChildren(children?.()).map((target, index) => getNewChild(target, index, props!))
 }
--- packages/util/src/props-util/index.ts
+++ packages/util/src/props-util/index.ts
@@ -1,4 +1,6 @@
-import { Comment, Fragment, Text } from 'vue'
+import type { VNode, VNodeNormalizedChildren } from 'vue'
+import { Comment, Fragment, isVNode, Text } from 'vue'
+import isValid from '../isValid'
 import initDefaultProps from './initDefaultProps'
 
 export function isEmptyElement(c: any) {
@@ -16,12 +18,40 @@ export function filterEmpty(children: any[] = []) {
       res.push(...child)
     else if (child?.type === Fragment)
       res.push(...filterEmpty(child.children))
-    else
-      res.push(child)
+    else res.push(child)
   })
   return res.filter(c => !isEmptyElement(c))
 }
 
-export {
-  initDefaultProps,
+export const skipFlattenKey = Symbol('skipFlatten')
+function flattenChildren(children?: VNode | VNodeNormalizedChildren, filterEmpty = true) {
+  const temp = Array.isArray(children) ? children : [children]
+  const res = []
+  temp.forEach((child) => {
+    if (Array.isArray(child)) {
+      res.push(...flattenChildren(child, filterEmpty))
+    }
+    else if (isValid(child)) {
+      res.push(child)
+    }
+    else if (child && typeof child === 'object' && child.type === Fragment) {
+      if (child.key === skipFlattenKey) {
+        res.push(child)
+      }
+      else {
+        res.push(...flattenChildren(child.children, filterEmpty))
+      }
+    }
+    else if (child && isVNode(child)) {
+      if (filterEmpty && !isEmptyElement(child)) {
+        res.push(child)
+      }
+      else if (!filterEmpty) {
+        res.push(child)
+      }
+    }
+  })
+  return res
 }
+
+export { flattenChildren, initDefaultProps }
